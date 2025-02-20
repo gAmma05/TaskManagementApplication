58f584ce984cb1cff49e7d8a47aee1c7d7e5160b
gAmma05 <minedung2005@gmail.com>
2025-02-20 10:38:47 +0700

    add regex for password/add delete and update user feature

--- src/java/dao/UserDAO.java
+++ src/java/dao/UserDAO.java
@@ -22,11 +22,11 @@ public class UserDAO {
 
     public boolean create(User u) {
         boolean status = false;
-        String sql = "INSERT INTO `User` (first_name, last_name, username, password, email, phone, role, created_at, updated_at)"
+        String sql = "INSERT INTO [User] (first_name, last_name, username, password, email, phone, role, created_at, updated_at)"
                 + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        try {
-            Connection con = DBConnection.getConnection();
+        try (Connection con = DBConnection.getConnection()) {
             PreparedStatement ps = con.prepareStatement(sql);
+
             ps.setString(1, u.getFirstName());
             ps.setString(2, u.getLastName());
             ps.setString(3, u.getUsername());
@@ -51,13 +51,86 @@ public class UserDAO {
         return status;
     }
 
+    public boolean updateBasic(User u) {
+        boolean status = false;
+        String sql = "UPDATE [User] SET firstName = ?, lastName = ?, email = ?, phone = ? WHERE user_id = ?";
+
+        try (Connection con = DBConnection.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, u.getFirstName());
+            ps.setString(2, u.getLastName());
+            ps.setString(3, u.getEmail());
+            ps.setString(4, u.getPhone());
+            ps.setInt(5, u.getUserId());
+
+            int rows = ps.executeUpdate();
+            if (rows > 0) {
+                status = true;
+            }
+            con.close();
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("DBUtils not found.");
+        } catch (SQLException ex) {
+            System.out.println("SQL Exception .Details: ");
+            ex.printStackTrace();
+        }
+        return status;
+    }
+
+    public boolean updatePass(User u) {
+        boolean status = false;
+        String sql = "UPDATE [User] SET password = ? WHERE user_id = ?";
+
+        try (Connection con = DBConnection.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, u.getPassword());
+            ps.setInt(2, u.getUserId());
+
+            int rows = ps.executeUpdate();
+            if (rows > 0) {
+                status = true;
+            }
+            con.close();
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("DBUtils not found.");
+        } catch (SQLException ex) {
+            System.out.println("SQL Exception .Details: ");
+            ex.printStackTrace();
+        }
+        return status;
+    }
+
+    public boolean delete(String userID) {
+        boolean status = false;
+        String sql = "DELETE FROM [User] WHERE user_id = ?";
+        try (Connection con = DBConnection.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, userID);
+            int rows = ps.executeUpdate();
+            if (rows > 0) {
+                status = true;
+            }
+            con.close();
+        } catch (ClassNotFoundException ex) {
+            System.out.println("DBUtils not found.");
+        } catch (SQLException ex) {
+            System.out.println("SQL Exception .Details: ");
+            ex.printStackTrace();
+        }
+        return status;
+    }
+
     public User getUser(String username, String password) {
         User u = null;
         String sql = "SELECT user_id, username, password, email, role "
-                + "FROM `User` u "
+                + "FROM [User] u "
                 + "WHERE username = ? AND password = ?";
-        try {
-            Connection con = DBConnection.getConnection();
+        try (Connection con = DBConnection.getConnection()) {
+
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, username);
             ps.setString(2, password);
@@ -82,26 +155,24 @@ public class UserDAO {
         }
         return u;
     }
-    
-    
-    public boolean setNewPass(String username, String newPassword){
+
+    public boolean setNewPass(String username, String newPassword) {
         boolean status = false;
         String sql = "UPDATE [User] SET password = ? WHERE username = ?";
-        try{
-            Connection con = DBConnection.getConnection();
+        try (Connection con = DBConnection.getConnection()) {
             PreparedStatement ps = con.prepareStatement(sql);
-            
+
             ps.setString(1, newPassword);
             ps.setString(2, username);
-            
+
             int rows = ps.executeUpdate();
-            
-            if(rows > 0){
+
+            if (rows > 0) {
                 status = true;
             }
-            
+
             con.close();
-        }catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex) {
             System.out.println("DBUtils not found.");
         } catch (SQLException ex) {
             System.out.println("SQL Exception. Details: ");
@@ -109,17 +180,16 @@ public class UserDAO {
         }
         return status;
     }
-    
+
     public User getUserByEmail(String email) {
         User u = null;
         String sql = "SELECT user_id, username, password "
                 + "FROM [User] u "
                 + "WHERE email = ?";
-        try {
-            Connection con = DBConnection.getConnection();
+        try (Connection con = DBConnection.getConnection()) {
+
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setString(1, email);
-            
 
             ResultSet rs = ps.executeQuery();
             if (rs != null) {
@@ -128,7 +198,7 @@ public class UserDAO {
                     u.setUserId(rs.getInt("user_id"));
                     u.setUsername(rs.getString("username"));
                     u.setPassword(rs.getString("password"));
-                    
+
                 }
             }
             con.close();
