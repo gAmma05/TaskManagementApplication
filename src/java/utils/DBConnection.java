885680c02df0b3693ad8bcb8a44eed0018f38d36
Nguyen Tri Dung <144704494+gAmma05@users.noreply.github.com>
2025-03-03 12:42:46 +0700

    Update DBConnection.java

--- src/java/utils/DBConnection.java
+++ src/java/utils/DBConnection.java
@@ -17,7 +17,7 @@ public class DBConnection {
 
     private static final String DB_NAME = "TMA";
     private static final String DB_USER_NAME = "root";
-    private static final String DB_PASSWORD = "start";
+    private static final String DB_PASSWORD = "1234";
 
     //connect ssms
 //    public static Connection getConnection() throws ClassNotFoundException, SQLException {
